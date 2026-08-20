7a7d9e2d8c790d0d7587347bc7e87b69a9c3440b
idrissi Mohammed <idmohame89@gmail.com>
2026-08-20 11:57:06 +0100

    feature:add Processing AI Recommendations...

--- aiservice/src/main/java/org/idrisophie/fitness/aiservice/services/ActivityAIService.java
+++ aiservice/src/main/java/org/idrisophie/fitness/aiservice/services/ActivityAIService.java
@@ -1,24 +1,134 @@
 package org.idrisophie.fitness.aiservice.services;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.idrisophie.fitness.aiservice.models.Activity;
+import org.idrisophie.fitness.aiservice.models.Recommendation;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Service
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @Slf4j
-@RequiredArgsConstructor
+@Service
 public class ActivityAIService {
     private final GeminiService geminiService;
 
-    public String generateRecommendation(Activity activity){
+    public ActivityAIService(GeminiService geminiService) {
+        this.geminiService = geminiService;
+    }
+
+    public Recommendation generateRecommendation(Activity activity){
         String prompt = createPromptForActivity(activity);
         String aiResponse = geminiService.getAnswer(prompt);
         log.info("RESPONSE FROM AI: {} ", aiResponse);
-        return aiResponse;
+        return processAiResponse(activity, aiResponse);
     }
-    
+    private Recommendation processAiResponse(Activity activity, String aiResponse) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(aiResponse);
+
+            JsonNode textNode = rootNode.path("candidate")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text");
+            String jsonContent = textNode.asText()
+                    .replaceAll("```json\\n", "")
+                    .replaceAll("\\n```", "")
+                    .trim();
+            //    log.info("RESPONSE FROM AI: {} ", jsonContent);
+
+            JsonNode analysisJson = mapper.readTree(jsonContent);
+            JsonNode analysisNode = analysisJson.path("analysis");
+            StringBuilder fullAnalysis = new StringBuilder();
+            addAnalysisSection(fullAnalysis, analysisNode, "overall", "Overall");
+            addAnalysisSection(fullAnalysis, analysisNode, "pace", "Pace");
+            addAnalysisSection(fullAnalysis, analysisNode, "heartRate", "Heart Rate");
+            addAnalysisSection(fullAnalysis, analysisNode, "caloriesBurned", "Calories");
+
+            List<String> improvements = extractImprovements(analysisJson.path("improvements"));
+
+            List<String> suggestions = extractSuggestions(analysisJson.path("suggestions"));
+
+            List<String> safety = extractSafetyGuideline(analysisJson.path("safety"));
+            return Recommendation.builder()
+                    .acvtivityId(activity.getId())
+                    .userId(activity.getUserId())
+                    .activityType(activity.getType())
+                    .recommendation(fullAnalysis.toString().trim())
+                    .improvements(improvements)
+                    .suggestions(suggestions)
+                    .safety(Arrays.asList(
+                            "Always warm up befor exercise",
+                            "Stay hydrated",
+                            "Listen to your body"
+                    ))
+                    .createdAt(LocalDateTime.now())
+                    .build();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new createDefaultRecommendation(activity);
+        }
+    }
+
+    private List<String> extractImprovements(JsonNode improvementsNode) {
+        List<String> improvements = new ArrayList<>();
+        if (improvementsNode.isArray()) {
+            improvementsNode.forEach(improvement -> {
+                String area = improvement.path("area").asText();
+                String detail = improvement.path("recommendation").asText();
+                improvements.add(String.format("%s: %s",area, detail));
+            });
+        }
+        return improvements.isEmpty() ?
+                Collections.singletonList("No Specific improvements provided"):
+                improvements;
+    }
+
+    private List<String> extractSuggestions(JsonNode suggestionNode) {
+        List<String> suggestions = new ArrayList<>();
+        if (suggestionNode.isArray()) {
+            suggestionNode.forEach(suggestion -> {
+                String workout = suggestion.path("workout").asText();
+                String description = suggestion.path("description").asText();
+                suggestions.add(String.format("%s: %s", workout, description));
+            });
+        }
+        return suggestions.isEmpty() ?
+                Collections.singletonList("No Specific suggestions provided") :
+                suggestions;
+    }
+
+    private List<String> extractSafetyGuideline(JsonNode safetyNode){
+        List<String> safety = new ArrayList<>();
+        if (safetyNode.isArray()) {
+            safetyNode.forEach(item -> safety.add(item.asText());
+        }
+                return safety.isEmpty() ?
+                        Collections.singletonList("No general safety provided") :
+                        safety;
+    }
+    private void addAnalysisSection(StringBuilder fullAnalysis,JsonNode analysisNode,String key,String prefix){
+        if(!analysisNode.path(key).isMissingNode()){
+            fullAnalysis.append(prefix)
+                    .append(analysisNode.path(key).asText())
+                    .append("\n\n");
+        }
+    }
+
+    private Recommendation createDefaultRecommendation(Activity activity){
+        return Recommendation.builder().build();
+    }
     private String createPromptForActivity(Activity activity){
         return String.format(""" 
                   Analyze this fitness activity and provide detailed recommendations in the following format
--- aiservice/src/main/java/org/idrisophie/fitness/aiservice/services/ActivityMessageListener.java
+++ aiservice/src/main/java/org/idrisophie/fitness/aiservice/services/ActivityMessageListener.java
@@ -1,6 +1,8 @@
 package org.idrisophie.fitness.aiservice.services;
 
 import org.idrisophie.fitness.aiservice.models.Activity;
+import org.idrisophie.fitness.aiservice.models.Recommendation;
+import org.idrisophie.fitness.aiservice.repositories.RecommendationRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -9,14 +11,20 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
-@RequiredArgsConstructor
 public class ActivityMessageListener {
-
     private final ActivityAIService aiService;
+    private final RecommendationRepository recommendationRepository;
+
+    public ActivityMessageListener(ActivityAIService aiService, RecommendationRepository recommendationRepository) {
+        this.aiService = aiService;
+        this.recommendationRepository = recommendationRepository;
+    }
 
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity){
         log.info("Received activity for processing: {}", activity.getId());
-        log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
+   //     log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
+        Recommendation recommendation = aiService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
     }
 }
